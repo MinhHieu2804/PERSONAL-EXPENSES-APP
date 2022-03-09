8f26ecc8a85ef12772dd24ff40745b395098f274
Minh Hieu <hieu0985792248@gmail.com>
2022-03-09 18:10:27 +0700

    basic functions

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function newTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.newTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount < 0) {
+      return;
+    }
+
+    newTx(enteredTitle, enteredAmount);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              // onChanged: (value) => titleInput = value,
+              controller: titleController,
+              onSubmitted: (_) => submitData(),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              // onChanged: (value) => amountInput = value,
+              controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
+            ),
+            FlatButton(
+                onPressed: submitData,
+                child: Text(
+                  'Add transaction',
+                  style: TextStyle(color: Colors.purple),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
